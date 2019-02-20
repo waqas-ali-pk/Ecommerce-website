1a6d77b66dc672a57f6779640fb3920689244ab9
waqas <waqasali.fastian@gmail.com>
2019-02-20 21:27:21 +0500

    Signup app added
    Signed-off-by: waqas <waqasali.fastian@gmail.com>

--- ecommerce/signup/views.py
+++ ecommerce/signup/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from ecommerce.signup.forms import SignUpForm
 
-# Create your views here.
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form':form})
