1d2ef5a772be784a99386fd3d20c8a88f5b4be11
waqas <lucifer501@yahoo.com>
2019-02-23 23:59:06 +0500

    login redirect issues fixed.
    Signed-off-by: waqas <lucifer501@yahoo.com>

--- ecommerce/signup/urls.py
+++ ecommerce/signup/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^$', RedirectView.as_view(url='home/')),
     url(r'^home/$', views.home, name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
